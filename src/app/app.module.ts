c90e755014b49aefbf9e5b807c8075cf764bc996
Hannes Kimara <hanneskimara@gmail.com>
2020-01-10 10:42:52 +0300

    Initilaize Quote,Quote Detail, Quote Form Components and Quote Class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { ElapsedTimePipe } from './elapsed-time.pipe';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    ElapsedTimePipe,
+    QuoteDetailComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
